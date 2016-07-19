1e02a6ea431cd25c1869ebcc4dacf5ea876ba3be
pedrosamaral <pedrosamaral@outlook.com>
2016-07-18 21:45:06 -0300

    Add files via upload

---
+++ MetodosDinamico.php
@@ -0,0 +1,31 @@
+<?php
+
+class MetodosDinamicos
+{
+    protected $data = [];
+    
+    public function __call($nome, $argumentos) 
+    {
+        $prefix = substr($nome, 0, 3);
+        $sufix = substr($nome, 2);
+        
+        if ($prefix == 'set')
+        {
+            $this->data[$sufix] = current($argumentos);
+        }
+        
+        if ($prefix == 'get') 
+        {
+            if(isset($this->data[$sufix]))
+            {
+                return $this->data[$sufix];
+            }
+        }
+        
+    }
+    
+    public function getNome()
+    {
+        return 'Pedro Paulo Amaral';
+    }
+}
